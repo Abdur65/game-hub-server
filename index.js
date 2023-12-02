3057f6e4afb46727fddf1a9eca950ef1088688e8
Abdur65 <abdurbc@gmail.com>
2023-12-03 00:50:32 +0600

    created API for teammates, team-collection both post and get

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-// parsing to json 
+// parsing to json
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqr8hsb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqr8hsb.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,52 +22,78 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // CRUD ops
-    const gameCollection = client.db("game-service").collection("games")
+    const gameCollection = client.db("game-service").collection("games");
+
+    // teammate collection
+    const teamCollection = client.db("game-service").collection("team");
 
     // API for creating a new game post.
-    app.post('/add-a-game', async(req, res) =>{
-        const game = req.body;
-        //Insert to MongoDB
-        const result = await gameCollection.insertOne(game)
-        
-        console.log.apply(req.body);
-        res.send(result);
+    app.post("/add-a-game", async (req, res) => {
+      const game = req.body;
+      //Insert to MongoDB
+      const result = await gameCollection.insertOne(game);
+
+      console.log.apply(req.body);
+      res.send(result);
     });
 
     // API for fetching all games
 
-    app.get("/all-games", async(req, res) => {
-      const result = await gameCollection.find().toArray()
+    app.get("/all-games", async (req, res) => {
+      const result = await gameCollection.find().toArray();
       res.send(result);
-    })
+    });
     //API for fetching single details
-    app.get("/game/:id", async(req, res) =>{
+    app.get("/game/:id", async (req, res) => {
       const id = req.params.id;
       //console.log(id);
       //find a game using id passed as param
-      const result = await gameCollection.findOne({_id: new ObjectId(id)});
+      const result = await gameCollection.findOne({ _id: new ObjectId(id) });
 
       res.send(result);
-    })
+    });
 
     // API for updating rows in the manageProduct table
     // put() used for U of the CRUD
-    app.put("/update-by-id/:id", async(req, res) =>{
+    app.put("/update-by-id/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedGame = req.body;
 
-      const updates = {$set:updatedGame}
+      const updates = { $set: updatedGame };
 
       // //Now call the update method for updating selecting query
       const result = await gameCollection.updateOne(filter, updates);
-      
+
+      res.send(result);
+    });
+
+    // API for deleting
+    app.delete("/delete-by-id/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      // calling the deleteone method
+      const result = await gameCollection.deleteOne(filter);
+
+      res.send(result);
+    });
+
+    // API for posting single teammate
+    app.post("/add-teammate", async(req, res) => {
+      const teammate = req.body;
+      const result = await teamCollection.insertOne(teammate);
+
+      res.send(result);
+
+    });
+    // get teammates
+    app.get("/all-teammates", async(req, res) =>{
+      const result = await teamCollection.find().toArray();
+
       res.send(result);
     })
-
   } finally {
-
-    
   }
 }
 run().catch(console.dir);
